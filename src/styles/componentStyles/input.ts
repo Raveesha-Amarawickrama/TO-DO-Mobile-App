261291d429ab8683090a4bc32e5e2ce4c2e167e7
Raveesha-Amarawickrama <amarawi-se21004@stu.kln.ac.lk>
2025-04-23 22:34:17 +0530

    Update input.ts

---
+++ src/styles/componentStyles/input.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  input: {
+    width: 280,
+    height: 38,
+    backgroundColor: '#242320',
+    borderWidth: 1,
+    borderColor: '#FF8303',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    fontSize: 14,
+    fontFamily: 'Roboto', 
+    fontWeight: '400',
+    color: '#F0E3CA', 
+  },
+});
+
+export default styles;
